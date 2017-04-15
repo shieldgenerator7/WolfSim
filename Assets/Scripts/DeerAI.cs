8146b4dc4246cf0514de20e358e25e0155fb2224
shieldgenerator7 <shieldgenerator7@yahoo.com>
2017-04-15 00:48:38 -0400

    Added Deer

---
+++ Assets/Scripts/DeerAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeerAI : MonoBehaviour {
+
+    public float speed = 0.04f;
+    public GameObject wolf;
+
+    private Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = transform.position - wolf.transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, ((Vector2)transform.position)+direction, speed);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed = 0.5f;//how fast the wolf can move
+    public float speed = 0.05f;//how fast the wolf can move
 
     public AudioSource woof;
     public AudioSource breathing;
     public AudioSource howl;
 
     public Vector2 targetLocation;
+    public GameObject targetObject;//the object it moves towards
 
     public bool enroute = false;
 
@@ -21,6 +22,10 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (targetObject != null)
+        {
+            targetLocation = targetObject.transform.position;
+        }
         if ((Vector2)transform.position != targetLocation)
         {
             if (!woof.isPlaying && !breathing.isPlaying)
@@ -39,6 +44,11 @@ public class PlayerController : MonoBehaviour {
 	}
     public void processTapGesture(Vector3 gpos)
     {
+        RaycastHit2D rch2d = Physics2D.Raycast(gpos, Vector3.zero);
+        if (rch2d && rch2d.collider.gameObject.GetComponent<DeerAI>() != null)
+        {
+            targetObject = rch2d.collider.gameObject;
+        }
         targetLocation = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         woof.Play();
         howl.Stop();
